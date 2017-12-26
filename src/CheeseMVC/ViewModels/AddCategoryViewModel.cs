c58aa33fbb54e35b2be77bfd87250b1ec1c6e3c9
alescareno <31868406+alescareno@users.noreply.github.com>
2017-12-26 13:23:58 -0600

    Add files via upload

---
+++ src/CheeseMVC/ViewModels/AddCategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCategoryViewModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Please provide a cheese category name good sir!")]
+        public string Name { get; set; }
+
+        public Cheese Cheeses { get; set; }
+
+    }
+}
+
---
+++ src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCheeseViewModel
+    {
+        [Required]
+        [Display(Name = "Cheese Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You must give your cheese a description")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please select a category sir!")]
+        [Display(Name = "Category")]
+        public int CategoryID { get; set; }
+
+        public List<SelectListItem> Categories { get; set; }
+        public CheeseCategory Category { get; set; }
+
+        public AddCheeseViewModel()
+        {
+
+            Categories = new List<SelectListItem>();
+
+            Categories.Add(new SelectListItem
+            {
+                Value = ((int)Category.ID).ToString(),
+                Text = Category.Name
+            });
+
+        }
+    }
+}
